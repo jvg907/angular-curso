53737a0e87f9ebc433043ac6a9e5cafc5d20e594
Juan Velasco <jvg907@inlumine.ual.es>
2025-01-10 00:30:15 +0100

    Fin de la seccion 4 del curso

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,36 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  standalone: false,
+
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  // Es una propiedad que realmente es un metodo
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  resetHero(): void {
+    this.name = 'Ironman';
+    this.age = 45;
+  }
+}
